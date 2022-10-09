9236d6aa40cd89ac34b92bc729debfc507ccbf1c
Alfaruq <114473569+alfaruqPS@users.noreply.github.com>
2022-10-09 20:22:50 +0700

    Add files via upload

---
+++ BIODATA.py
@@ -0,0 +1,9 @@
+nama = "ABU NUR AL-FARUQ"
+
+kelas = 'TI22E'
+
+jurusan = 'teknik informatika'
+
+quote = 'maju terus jangan pernah berhenti'
+
+print ('nama =', nama, '\n', 'kelas = ', kelas, '\n' 'jurusan =', jurusan, '\n' 'quote', quote, '\n')
